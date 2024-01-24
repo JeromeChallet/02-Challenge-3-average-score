a2981b8057b1e303cb26c8ac149a03fd3d6c68f1
jrme421 <jeromepchallet@gmail.com>
2024-01-24 22:17:25 +0900

    average score

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Write your code below. Good luck! 🙂 */
+
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
